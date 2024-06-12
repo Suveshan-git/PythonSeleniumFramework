e7ea8a4219ba058c4098bfe90da6024ee5e6c069
Suveshan <suveshan.moodley@gmail.com>
2024-06-12 02:21:13 +0200

    Added README

--- tests/test_login.py
+++ tests/test_login.py
@@ -12,21 +12,16 @@ def setup():
     driver = DriverFactory.get_driver(config['DEFAULT']['browser'])
     driver.get(config['DEFAULT']['base_url'])
     yield driver
-    time.sleep(2)
     DriverFactory.quit_driver(driver)
 
 
 def test_login_valid_user(setup):
     try:
         login_page = LoginPage(setup)
-        time.sleep(2)
         login_page.enter_username('standard_user')
-        time.sleep(2)
         login_page.enter_password('secret_sauce')
-        time.sleep(2)
         login_page.take_screenshot("test_login_valid_user_information")
         login_page.click_login()
-        time.sleep(2)
         assert login_page.get_title() == 'Swag Labs'
         login_page.take_screenshot("test_login_valid_title")
 
